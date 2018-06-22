dfcd2c83278bcf09ea6496c5cbdfe77b2e8e75e0
guaracyalima <guaracyaraujolima@gmail.com>
2018-06-22 13:31:36 -0300

    incrementa entregas

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('authenticate', 'AuthController@auth');
 Route::group(['middleware' => ['auth:api']], function () {
     Route::get('opiniao', 'TableController@index');
+    Route::get('dtvisitaobra', 'TableController@visit');
+    Route::get('dtdesligamento', 'TableController@shutdown_by_date');
 });
 
